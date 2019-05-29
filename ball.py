d2586bb599aab77f84b2865fa84dd18fdb6fe3a4
Polowis <39208974+polowis@users.noreply.github.com>
2019-05-29 11:15:21 +1000

    Initial commit

--- ball.py
+++ ball.py
@@ -8,5 +8,23 @@ class Ball:
         
     '''
     Use recursive algorithm to move the position of the ball
+    This will likely result in stack overflow, since the ball will need to move infinitely until one player lose. 
+    This piece of code uses tail recursion
     '''
+    def update(self, x=2, y=2, z=0):
+        z += 1
+        display.set_pixel(self.x, self.y, 9)
+        sleep(500)
+        display.set_pixel(self.x, self.y, 0)
+        if z <= 200:
+          if self.x == 4:
+            self.x -= 1
+            self.y += 1
+          elif self.y == 0:
+            self.y += 1
+            self.x -= 1
+          else:
+            self.x += 1
+            self.y -= 1
+        self.update(self.x, self.y, z)
         
