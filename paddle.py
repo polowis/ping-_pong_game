a80e62d6328a8712bf584747611293d9d0e502b9
Polowis <39208974+polowis@users.noreply.github.com>
2019-06-04 14:22:04 +1000

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,19 +1,22 @@
 class Paddle():
-    def __init__(self, x1, x2 , y1, y2 ):
+    def __init__(self, x1, x2 , y1, y2):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
-
-    def setPaddleRight(self):
+        
+    #private method
+    def __setPaddleRight(self):
         self.x1 = 4
         self.x2 = 4
         self.y1 = 0
         self.y2 = 1
         display.set_pixel(self.x1, self.y1, 9)
         display.set_pixel(self.x2, self.y2, 9)
-
-    def setPaddleLeft(self):
+     
+     #private method
+    
+    def __setPaddleLeft(self):
         self.x1 = 0
         self.x2 = 0
         self.y1 = 4
@@ -36,8 +39,6 @@ class Paddle():
         self.y += 1
         if self.y1 or self.y2 == 0:
             return self.y1, self.y2
-            
-
 
     def getCurrentPosition(self):
         return self.x1, self.y1, self.x2, self.y2
