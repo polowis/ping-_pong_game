b8def3aa5d36ede0334c74dd34f01cc1e0bdf0e2
Polowis <39208974+polowis@users.noreply.github.com>
2019-06-04 14:10:19 +1000

    paddle2.py

--- paddle.py
+++ paddle.py
@@ -1,7 +1,50 @@
 class Paddle():
-    def setPaddleLeft(self):
-        display.set_pixel(4, 0, 9)
-        display.set_pixel(4, 1, 9)
+    def __init__(self, x1, x2 , y1, y2 ):
+        self.x1 = x1
+        self.y1= y1
+        self.x2 = x2
+        self.y2 = y2
+
     def setPaddleRight(self):
-        display.set_pixel(0, 4, 9)
-        display.set_pixel(0, 3, 9)
+        self.x1 = 4
+        self.x2 = 4
+        self.y1 = 0
+        self.y2 = 1
+        display.set_pixel(self.x1, self.y1, 9)
+        display.set_pixel(self.x2, self.y2, 9)
+
+    def setPaddleLeft(self):
+        self.x1 = 0
+        self.x2 = 0
+        self.y1 = 4
+        self.y2 = 3
+        display.set_pixel(self.x1, self.y1, 9)
+        display.set_pixel(self.x2, self.y2, 9)
+
+    def startGame(self):
+        self.setPaddleLeft()
+        self.setPaddleRight()
+
+    def moveUp(self):
+        self.y1 -= 1
+        self.y2 -= 1
+        if self.y1 or self.y2 == 0:
+            self.y1 = self.y1
+            self.y2 = self.y2
+
+    def moveDown(self):
+        self.y += 1
+        self.y += 1
+        if self.y1 or self.y2 == 0:
+            self.y1 = self.y1
+            self.y2 = self.y2
+
+
+    def getCurrentPosition(self):
+        return self.x1, self.y1, self.x2, self.y2
+
+
+    def update(self):
+        display.set_pixel(self.x1, self.y1, 9)
+        display.set_pixel(self.x2, self.y2, 9)
+
