1594d8d6a876ab79b895e2142e22018025f1ce0f
Polowis <39208974+polowis@users.noreply.github.com>
2019-06-14 12:55:09 +1000

    Update getting close to done.py

--- getting close to done.py
+++ getting close to done.py
@@ -6,42 +6,58 @@ import radio
 radio.on()
 radio.config(channel = 23) #The channel makes sure that the code is received without being lost and mixed up with other radio signals.
 
+#group all the functions in a class, easier to maintain and structure the code. 
+#camel style for naming convetion :). 
 class Ball:
+    #create a constructor, takes 4 parameters
+    #the x and y coordinates gives position of the ball. 
+    #xspeed and yspeed are random values which determine the movement of the ball.
+    #here, self is the instance of the class
     def __init__(self, x = 2, y = 2, xspeed = (random.choice([-1, 1])), yspeed = (random.choice([-1, 1]))): 
         self.x = x
         self.y = y
         self.xspeed = xspeed
         self.yspeed = yspeed
         self.bottomPaddle = 1
-        self.topPaddle = 1
-        self.yspeed = (random.choice([-1, 1]))
-        self.xspeed = (random.choice([-1, 1]))
-
+        self.topPaddle = 
+        
+    #set the top paddle at specific position.
+    #paddle has 2 pixels, displaying on the top of the x-axis. 
+    #set this to private method, so it cannot be accessed outside of the class
     def __setPaddleTop(self):
-        display.set_pixel(self.topPaddle, 0, 5)
-        display.set_pixel((self.topPaddle - 1), 0, 5)
-
+        display.set_pixel(self.topPaddle, 0, 5) 
+        display.set_pixel((self.topPaddle - 1), 0, 5) 
+    
+    #set the bottom padde
+    #paddle has 2 pixels, displaying on the bottom of the x-axis
+    #again, set this method to private, so it cannot be accessed outside of the class
     def __setPaddleBottom(self):
         display.set_pixel(self.bottomPaddle, 4, 5)
         display.set_pixel((self.bottomPaddle - 1), 4, 5)
-
+    
+    #return the x and y value of the ball
     def __getCurrentBallPosition(self):
         return self.x, self.y
     
+    #check condition of the paddle, if bottom paddle position less than 1, assign 1 to its value
+    #prevents index out of bounds
     def __getSetPaddle(self):
         if self.bottomPaddle < 1:
             self.bottomPaddle = 1
         elif self.bottomPaddle > 4:
             self.bottomPaddle = 4
-
+    
+    #the setPaddle method takes 1 parameter which is the message
     def setPaddle(self, message): #(SetPaddle) reads the message that is sent from the movement microbit and is used to move the paddle either left or right.
         if message == "right":
             self.bottomPaddle += 1
+            #move the paddle by one according to the message
         elif message == "left":
             self.bottomPaddle -= 1
         self.__getSetPaddle()
         return
-
+    
+    #display all the paddle, use private method setPaddleTop and setPaddleBottom
     def show(self):
         display.clear()
         self.__setPaddleTop()
@@ -52,19 +68,22 @@ class Ball:
 
     def AI(self): #The AI is displayed on the top of the microbit display and reacts to the movement of the ball and this is what the player is playing against.
         if (random.randint(1, 6)) != 1:
+            #move the top padding according to the ball position
             if self.x < self.topPaddle - 1:
                 self.topPaddle = self.topPaddle -1
             elif self.x > self.topPaddle:
                 self.topPaddle = self.topPaddle + 1
-              
+    
+    #render the ball on the microbit
     def render(self):
-      self.__getCurrentBallPosition()
-      display.set_pixel(self.x, self.y, 9)
+      self.__getCurrentBallPosition() #this is for debugging, to make sure the ball doesnt go out of bounds. 
+      #if no errors occur, display the ball.
+      display.set_pixel(self.x, self.y, 9) 
       sleep(1000)
       display.set_pixel(self.x, self.y, 0)
       sleep(1000)
     
-        
+    #up    
     def update(self):
         if self.y == 2:
             self.x = self.x + self.yspeed
